cb3d9fee549eda0af10cf7868686121707c948e5
Trung Minh Le <trungleit0108@gmail.com>
2022-08-19 20:55:55 +0700

    keep login after user refresh

--- client/src/auth/Auth.js
+++ client/src/auth/Auth.js
@@ -15,7 +15,7 @@ export default class Auth {
   });
 
   constructor(history) {
-    this.history = history
+    this.history = history;
 
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
@@ -33,8 +33,8 @@ export default class Auth {
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        console.log('Access token: ', authResult.accessToken)
-        console.log('id token: ', authResult.idToken)
+        console.log('Access token: ', authResult.accessToken);
+        console.log('id token: ', authResult.idToken);
         this.setSession(authResult);
       } else if (err) {
         this.history.replace('/');
@@ -53,14 +53,21 @@ export default class Auth {
   }
 
   setSession(authResult) {
-    // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
-
     // Set the time that the access token will expire at
-    let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
     this.accessToken = authResult.accessToken;
     this.idToken = authResult.idToken;
     this.expiresAt = expiresAt;
+
+    // Set isLoggedIn flag in localStorage
+    localStorage.setItem(
+      'isLoggedIn',
+      JSON.stringify({
+        accessToken: this.accessToken,
+        idToken: this.idToken,
+        expiresIn: authResult.expiresIn
+      })
+    );
 
     // navigate to the home route
     this.history.replace('/');
@@ -68,13 +75,15 @@ export default class Auth {
 
   renewSession() {
     this.auth0.checkSession({}, (err, authResult) => {
-       if (authResult && authResult.accessToken && authResult.idToken) {
-         this.setSession(authResult);
-       } else if (err) {
-         this.logout();
-         console.log(err);
-         alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
-       }
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      } else if (err) {
+        this.logout();
+        console.log(err);
+        alert(
+          `Could not get a new token (${err.error}: ${err.error_description}).`
+        );
+      }
     });
   }
 
